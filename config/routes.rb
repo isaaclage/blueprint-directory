ccc2cf305835097897e72b1a01d03ff660e4eedb
Erika Lage <el1546@nyu.edu>
2015-08-12 15:35:42 -0400

    Play with layout

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  'static_pages/help'
   get  'static_pages/about'
+  get  'users/user_id'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
