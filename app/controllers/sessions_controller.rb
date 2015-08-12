70a0f5c1f5b0dd19b89e6b6b7b84103790bba0c8
Erika Lage <el1546@nyu.edu>
2015-08-12 15:23:28 -0400

    Add dropdown to user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to root_url
     else
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
