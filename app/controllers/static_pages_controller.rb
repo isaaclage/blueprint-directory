33efe70d85ca0e3d1a7fc08b9b54a03d0e7e791f
Erika Lage <el1546@nyu.edu>
2015-08-12 20:18:04 -0400

    add status bar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   
   def home
     @categories = Resource.names
+    @comments = Comment.order("created_at desc").limit(5)
   end
   
   def help
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @comments = @user.comments.all
   end
   
   def new
