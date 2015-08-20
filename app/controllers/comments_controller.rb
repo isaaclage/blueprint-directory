c45aa0b683aa00cf4bb9f6158ddcf57253375842
Erika Lage <el1546@nyu.edu>
2015-08-20 13:59:49 -0400

    Fix comments user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   def create
     @resource = Resource.find(params[:resource_id])
     @comment = @resource.comments.create(comment_params)
+    @user = @comment.user_id
     redirect_to resource_path(@resource)
   end
   
